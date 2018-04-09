0050e73395750124e22443dae078d9a412ce468c
Mark Hendriks <mark.hendriks9999@gmail.com>
2018-04-09 09:45:40 +0200

    oops

---
+++ app/Http/Controllers/TaskEditController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mark
+ * Date: 19-2-2018
+ * Time: 20:06
+ */
+
+namespace App\Http\Controllers;
+
+
+use App\WebsiteText;
+use Illuminate\Http\Request;
+
+class TaskEditController
+{
+    function index(){
+
+    }
+    function store(){
+
+    }
+}
