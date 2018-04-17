ffaa31a53f4f321c429a00588ebea3794fccb4ce
MariusHendriks <Mark.hendriks9999@gmail.com>
2018-04-17 22:00:32 +0200

    no qlue what this is

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,9 @@ Route::get('/courses/add/', ['uses' => 'AddTaskController@create', 'as' => 'addT
 Route::post('/courses/add/', ['uses' => 'AddTaskController@store', 'as' => 'addTask']);
 Route::get('/courses/{course}', ['uses' => 'CourseController@index']);
 Route::get('/courses/{course}/{task}', ['uses' => 'TaskController@index', 'as' => 'taskIndex']);
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/courses/{course}/{title}/edit', ['uses' => 'EditTaskController@index']);
 Route::post('/courses/{course}/{title}/edit', ['uses' => 'EditTaskController@store']);
-
 Route::get('/php/', ['uses' => 'PhpExample@index']);
-
 Route::get('/courses/{course}/{title}', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
