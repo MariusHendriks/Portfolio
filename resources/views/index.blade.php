0bbc87caba18311b5eb3246359c7d0224b5c8068
MariusHendriks <Mark.hendriks9999@gmail.com>
2018-04-17 15:28:27 +0200

    mayor style changes and homepage done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,33 +1,95 @@
-
 {{-- Zorgt ervoor dat je de 'main' verder bouwd main.blade.php --}}
 @extends('main')
 
 {{-- Veranderd de titel van courses met de blade-manier van laravel --}}
 @section('title')
-	Courses
+    Courses
 @endsection
 
 {{-- Zorgt ervoor dat er doorgebouwd word op de plaats van @yield van de main.blade.php --}}
 @section('Main')
-<section class="homepage">
-	<div class="container">
-		<div class="row">
-			<div class="col s5 homepageContent">
-				<h1>Welkom!</h1>
-				<p>
-					Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.
-				</p>
-				<a href="/courses">
-					<button class="btn waves-effect waves-light default-btn homepagebutton">Naar Vakken!
-							<i class="material-icons right">send</i>
-					</button>
-				</a>
-			</div>
-			<div class="col s4 push-s3">
-				<img class="homepagePhoto" src="{{asset('photos/shares/me.png')}}">
-			</div>
-		</div>
-	</div>
-</section>
+    <section class="homepage">
+        <div class="container centertext">
+            <div class="row">
+                <div class="col s12 text-center">
+                    <img class="indexImage" src="{{asset('photos/shares/me/indeximage.png')}}">
+                    <p class="indexText">
+                        Mijn naam is
+                    </p>
+                    <p class="indexTextMe">
+                        Mark Hendriks
+                    </p>
+                </div>
+            </div>
+            <div class="changePart">
+                <div class="andima">
+                    <span class="ima">
+                        En ik ben
+                    </span>
+                </div>
+                 <div class="words">
+                    <span class="change ima">
+                        een designer
+                    </span>
+                </div>
+            </div>
+            <div class="homepagebutton">
+                <a href="/courses">
+                    <button class="btn waves-effect waves-light default-btn homepage-btn">Naar Vakken!
+                        <i class="material-icons right">send</i>
+                    </button>
+                </a>
+            </div>
+        </div>
 
+    </section>
+    <script>
+        {{-- Alles tot aan een
+        designer komt van mezelf
+        af. De rest zijn allemaal
+        van mensen waar ik het
+        aan gevraagd heb.--}}
+        var words = [
+            'een developer',
+            'een student',
+            'een gamer',
+            'een gitarist',
+            'creatief',
+            'een designer',
+            'klunsig',
+            'sociaal',
+            'lang',
+            'eigenwijs',
+            'sarcastisch',
+            'gemotiveerd',
+            'leergierig',
+            'zelfstandig',
+            'aardig?',
+            'loyaal',
+            'hard werkend',
+            'Shrek',
+            'vriendelijk',
+            'Jeff',
+            'ervaren',
+            'een grappenmaker',
+            'een sfeerbrenger',
+            'een fixer',
+            'behulpzaam',
+            'betrouwbaar',
+            'een leider',
+            'een memelord'
+        ];
+
+        var getRandomWord = function () {
+            return words[Math.floor(Math.random() * words.length)];
+        };
+        $(function () { // after page load
+            setInterval(function () {
+                $('.change').fadeOut(500, function () {
+                    $(this).html(getRandomWord()).fadeIn(500);
+                });
+                // 5 seconds
+            }, 3000);
+        });
+    </script>
 @endsection
