0a657f8930d2a2250fe197fb826c6cf1c44588bc
Mark Hendriks <mark.hendriks9999@gmail.com>
2018-05-17 11:24:20 +0200

    carousel nav

--- resources/views/carrousel.blade.php
+++ resources/views/carrousel.blade.php
@@ -1,21 +1,25 @@
 <?php $sprintNumber = $sprint->first()->sprint ?>
-<div class="owl-carousel sprint{{$sprintNumber}}">
-
+<div class="owl-carousel  owl-theme owl-loaded sprint{{$sprintNumber}}">
 </div>
+<div class="navload"></div>
+
 <script>
     $(document).ready(function () {
         @foreach($sprint as $item)
-            $(".sprint{{$sprintNumber}}").append("<a href='{{route('taskIndex', ['course' => $item->course, 'task' => $item->title])}}'> <div id='item-{{ $item->id }}' class='textblok' style='background-image:url({{ $item->photopath }});' data-merge='{{ $item->proudness }}'> <div class='centertext'>{{$item->title}}</div></div></a> ");
-
+          $(".sprint{{$sprintNumber}}").append("<a href='{{route('taskIndex', ['course' => $item->course, 'task' => $item->title])}}'> <div id='item-{{ $item->id }}' class='textblok' style='background-image:url({{ $item->photopath }});' data-merge='{{ $item->proudness }}'> <div class='centertext'>{{$item->title}}</div></div></a> ");
         @endforeach
+
+
         $(".sprint{{$sprintNumber}}").owlCarousel({
+            nav: true,
+            center: true,
+            dots: true,
             items: 1,
             lazyLoad: true,
             nav:false,
             margin:10,
             merge: true,
             loop: true,
-            nav: true,
             responsiveClass:true,
             responsive:{
                 0:{
@@ -29,6 +33,10 @@
                     items:7
                 }
             }
+        });
+        $('.owl-carousel').find('.owl-dots').removeClass('disabled');
+        $('.owl-carousel').on('changed.owl.carousel', function(event) {
+          $(this).find('.owl-dots').removeClass('disabled');
         });
     });
 </script>
