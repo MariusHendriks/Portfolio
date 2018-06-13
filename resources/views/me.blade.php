7b6bc26f5e62daa6601e840e862f81ab5615ca91
Mark Hendriks <mark.hendriks9999@gmail.com>
2018-06-13 10:51:10 +0200

    update me page

--- resources/views/me.blade.php
+++ resources/views/me.blade.php
@@ -10,27 +10,48 @@
 {{-- Zorgt ervoor dat er doorgebouwd word op de plaats van @yield van de main.blade.php --}}
 @section('Main')
 	<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
-	<section class="guitar">
-		<style>
-
-		</style>
-		</head>
-		<body>
-
-		<h1>Grid Layout</h1>
-
-		<p>This grid layout contains six columns and three rows:</p>
-
-		<div class="grid-container">
-		  <div class="item1">Head</div>
-		  <div class="item2">Menu</div>
-		  <div class="item3">Main</div>
-		  <div class="item4">Right</div>
-		  <div class="item5">Footer</div>
+	<section class="gitaar-outter">
+		<div class="gitaar">
+			<div class="head"></div>
+			<div class="fret1"></div>
+			<div class="fret2"></div>
+			<div class="fret3"></div>
+			<div class="fret4"></div>
+			<div class="fret5"></div>
+			<div class="fret6"></div>
+			<div class="fret7"></div>
+			<div class="fret8"></div>
+			<div class="fret9"></div>
+			<div class="fret10"></div>
+			<div class="fret11"></div>
+			<div class="fret12"></div>
+			<div class="fret13"></div>
+			<div class="fret14"></div>
+			<div class="fret15"></div>
+			<div class="fret16"></div>
+			<div class="fret17"></div>
+			<div class="text1 textblock rightarrow"></div>
+			<div class="text2 textblock leftarrow"></div>
+			<div class="text3 textblock rightarrow"></div>
+			<div class="text4 textblock leftarrow"></div>
+			<div class="text5 textblock rightarrow"></div>
+			<div class="text6 textblock leftarrow"></div>
+			<div class="text7 textblock rightarrow"></div>
+			<div class="text8 textblock leftarrow"></div>
+			<div class="text9 textblock rightarrow"></div>
+			<div class="text10 textblock leftarrow"></div>
+			<div class="text11 textblock rightarrow"></div>
+			<div class="text12 textblock leftarrow"></div>
+			<div class="text13 textblock rightarrow"></div>
+			<div class="text14 textblock leftarrow"></div>
+			<div class="text15 textblock rightarrow"></div>
+			<div class="text16 textblock leftarrow"></div>
+			<div class="text17 textblock rightarrow"></div>
 		</div>
+	</section>
 <script>
-window.sr = ScrollReveal({ reset: true });
-sr.reveal('.chat');
+	window.sr = ScrollReveal({ reset: true });
+	sr.reveal('.textblock');
 </script>
 
 @endsection
